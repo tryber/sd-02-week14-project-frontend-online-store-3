92effb12d253e937681b7418d589e1f31e98f46c
guicgs <etohlif@protonmail.com>
2020-02-17 17:55:02 -0300

    primeira versão componente ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,1 +1,31 @@
-ssss
+import React, { Component } from 'react';
+import ProductItem from './ProductItem';
+import * as ProductAPI from '../services/productAPI';
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    }
+  }
+
+  componentDidUpdate() {
+    ProductAPI.getCategories()
+      .then(products => this.setState({ products }));
+    ProductAPI.getQueryCategory()
+      .then(products => this.setState({ products }));
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div>
+        {products.map(product => <ProductItem product={product} />)}
+      </div>
+    );
+  }
+}
+
+export default ProductList;
