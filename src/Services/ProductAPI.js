e31772c646ab1374f80356167fe96068c1507fba
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-17 16:42:44 -0300

    Finalizado o API

--- src/Services/ProductAPI.js
+++ src/Services/ProductAPI.js
@@ -1,4 +1,27 @@
 
-export const getCategories = () => {
-  
+export async function getCategories() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+  const data = await response.json();
+  return data;
+}
+
+export async function getCategorie(categorieId) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+
+export async function getQuery(query) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+
+export async function getQueryNCategorie(query, categorieId) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}&q=${query}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
 }
