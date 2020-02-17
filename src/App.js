bee3d1d3e48ac3477d0a687a0b883c84d10f4ea9
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-17 17:30:40 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-// import React from 'react';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import SearchBar from './pages/SearchBar';
-// import ProductDetails from './pages/ProductDetails';
-// import ShoppingCart from './pages/ShoppingCart';
-// import NotFound from './pages/NotFound';
-// import './App.css';
+import React from 'react';
+import './App.css';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <main>
-//         <Switch>
-//           <Route exact path="/" component={SearchBar} />
-//           <Route exact path="/products/:id" component={ProductDetails} />
-//           <Route exact path="/shoppingcart" component={ShoppingCart} />
-//           <Route component={NotFound} /> 
-//         </Switch>
-//       </main>
-//     </BrowserRouter>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <p>teste</p>
+    </div>
+  );
+}
 
-//
-// export default App;
+export default App;
---
+++ src/pages/README.md
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import SearchBar from './pages/SearchBar';
+// import ProductDetails from './pages/ProductDetails';
+// import ShoppingCart from './pages/ShoppingCart';
+// import NotFound from './pages/NotFound';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <main>
+//         <Switch>
+//           <Route exact path="/" component={SearchBar} />
+//           <Route exact path="/products/:id" component={ProductDetails} />
+//           <Route exact path="/shoppingcart" component={ShoppingCart} />
+//           <Route component={NotFound} /> 
+//         </Switch>
+//       </main>
+//     </BrowserRouter>
+//   );
+// }
+
+//
+// export default App;
