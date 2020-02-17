f7ceebd36452eefb9cb4ee7b0f732451a14dee55
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-17 16:48:02 -0300

    Finalizado API e arrumado nomes das pastas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import * as ProductAPI from './Services/ProductAPI';
 
 function App() {
-
-  ProductAPI.getCategories()
-  .then (categories => console.log(categories))
-
-  ProductAPI.getQueryNCategorie('Motorola', 'MLB1055')
-  .then (data => data.results.map(({title, thumbnail, price }) => console.log(title, thumbnail, price) ))
-
   return (
     <div>
-      
+      <p>teste</p>
     </div>
   );
 }
