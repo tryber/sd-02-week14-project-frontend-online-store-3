9c13f3f7ab820b8aaac454052fccf1b9d4adbc59
guicgs <etohlif@protonmail.com>
2020-02-17 19:03:42 -0300

    ProductItem pronto

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,1 +1,20 @@
-ss
+import React, { Component } from 'react';
+
+class ProductItem extends Component {
+  render() {
+    const { title, thumbnail, price } = this.props;
+    return (
+      <div>
+        <div>
+          {title}
+        </div>
+        <div>
+          <img src={thumbnail} alt={`imagem de um ${title}`} />
+        </div>
+          R$ {price}
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
