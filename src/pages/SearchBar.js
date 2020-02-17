6d3d6e46bacabb6d56783cd6bc834c8205836b5a
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-17 18:16:47 -0300

    fazendo search bar

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,1 +1,69 @@
-as
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      selectedCategory: '',
+    };
+    this.createInputSearch = this.createInputSearch.bind(this);
+    this.createGender = this.createGender.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    const { value } = event.target;
+    this.setState({ searchText: value });
+  }
+  
+  onSelectedCategoryChange(event) {
+    const { value } = event.target;
+    this.setState({ selectedCategory: value });
+
+  createInputSearch() {
+    return (
+      <label htmlFor="text">
+        <input
+          id="text"
+          type="text"
+          value={searchText}
+          onKeyUp={onSearchTextChange}
+        />
+      </label>
+    );
+  }
+
+  createCategory() {    
+    return (
+      <label htmlFor="category">
+        Categorias
+        <select
+          id="gender"
+          value={selectedCategory}
+          onChange={onSelectedCategoryChange}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+
+    render() {
+      return (
+        <form>
+          {this.createInputSearch()}
+          <br />
+          {this.createCheckbox()}
+          <br />
+          {this.createGender()}
+          <br />
+        </form>
+      );
+    }
+  }
+
+  export default SearchBar;
