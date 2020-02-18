111ea0863b65c9267cdd41033e30d94ebe450032
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-18 19:36:24 -0300

    adicionar Router no APP, Adicionado page NotFound, Adicionado layout shoppingCart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SearchBar from './pages/SearchBar';
+import ShoppingCart from './pages/ShoppingCart';
+import NotFound from './pages/NotFound';
 import './App.css';
 // import ProductList from './components/ProductList';
 
 function App() {
   return (
-    <div>
-      <SearchBar />
-    </div>
+    <main>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SearchBar} />
+          <Route path="/shopping_cart" component={ShoppingCart} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,1 +1,9 @@
-a
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return <div className="not-found header">Página não encontrada</div>;
+  }
+}
+
+export default NotFound;
