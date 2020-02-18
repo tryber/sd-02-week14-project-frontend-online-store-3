4515c071cf5bb67531b6055b3d213da4e4bba185
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-18 15:39:17 -0300

    Iniciando o carrinho de compras

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,1 +1,13 @@
-a
+import React from 'react';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
