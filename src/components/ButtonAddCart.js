6e79307c953774e08dab70f2699cedaf310c1c69
guicgs <etohlif@protonmail.com>
2020-02-18 15:33:39 -0300

    trabalhando no botão de adicionar ao carrinho

--- src/components/ButtonAddCart.js
+++ src/components/ButtonAddCart.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 
 class ButtonAddCart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: '',
+    };
+  }
   render() {
     <button></button>
   }
