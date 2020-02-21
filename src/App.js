31638eedeb2dbdf131871bda21715800aa49f64b
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-21 18:18:51 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Checkout from './pages/Checkout';
-// import SearchBar from './pages/SearchBar';
 import ShoppingCart from './pages/ShoppingCart';
 import NotFound from './pages/NotFound';
 import ProductDetails from './pages/ProductDetails';
-import Checkout from './pages/Checkout';
+import SearchBar from './pages/SearchBar';
 import './App.css';
-// import ProductList from './components/ProductList';
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Checkout} />
+          <Route exact path="/" component={SearchBar} />
           <Route path="/products/:id" component={ProductDetails} />
           <Route path="/shopping_cart" component={ShoppingCart} />
           <Route path="/checkout" component={Checkout} />
