4eeaa88ce8789c1e12087c74390fb31f757f2b5d
guicgs <etohlif@protonmail.com>
2020-03-01 20:17:08 -0300

    correções CC

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -25,12 +25,14 @@ class ProductList extends Component {
   render() {
     const { products } = this.state;
     const { categoryId, query, totalItems } = this.props;
-    if (!products)
-      return <h2 className="empty_list">Digite algum termo de pesquisa ou escolha uma categoria.</h2>;
-
-    if (products.length === 0)
+    if (!products) {
+      return (
+        <h2 className="empty_list">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+      );
+    }
+    if (products.length === 0) {
       return <h2 className="empty_list">Nenhum produto foi encontrado.</h2>;
-
+    }
     return (
       <div className="product-list">
         {products.map((product) => (
